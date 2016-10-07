575514e8b9168153ef2b916a63ec1257aed60f3d
liangmbs <chishengliang@ChiShengs-MacBook-Pro.local>
2016-10-07 22:29:51 +0800

    first hello world

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+//import App from './App';
+//import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    //<App />
+    <h1> Hello, world! </h1>,
+    document.getElementById('example')
 );
