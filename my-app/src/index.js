235094b6173e43bcf4fe283274d2ca2a5f861f9e
liangmbs <chishengliang@ChiShengs-MacBook-Pro.local>
2016-10-08 15:08:54 +0800

    adding the drop down menu by using react-select

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,37 @@
+//ReactJS
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//GraphiQl
+//import GraphiQL from 'graphiql';
+//import fetch from 'isomorphic-fetch';
+
 //import App from './App';
 //import './index.css';
 
+//Select-react
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+var options = [
+    { value: 'one', label: 'One' },
+    { value: 'two', label: 'Two' },
+    { value: 'three', label: 'Three'}
+];
+
+function logChange(val) {
+    console.log("Selected: " + val);
+};
+
+
+
 ReactDOM.render(
-    //<App />
-    <h1> Hello, world! </h1>,
-    document.getElementById('example')
-);
+    <Select
+        name="form-field-name"
+        value="one"
+        options={options}
+        onChange={logChange}
+    />,
+    document.getElementById('input1')
+)
+
